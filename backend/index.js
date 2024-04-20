6667ca123c76ff4b00241a181eefd47bc5afa371
mayank <maysmayank@gmail.com>
2024-04-20 11:42:01 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express=require("express")
+const app=express();
+app.use(express.json());
+
+app.post("/todo",(req,res)=>{
+
+})
+app.get("/todos",(req,res)=>{
+
+})
+app.get("/completed",(req,res)=>{
+    
+})
